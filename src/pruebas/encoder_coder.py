94aba4fe155490f8d8f1245f931f080f3abf0621
leoborgnino <leoborgnino@gmail.com>
2023-09-20 11:44:56 -0300

    Todos los sensores listos

--- src/pruebas/encoder_coder.py
+++ src/pruebas/encoder_coder.py
@@ -1,3 +1,7 @@
+#75 pulsos por vuelta cada interrupcion
+#60 mm de diametro c/ rueda
+#2.512 mm avanzados de forma lineal por pulso medido
+
 import RPi_map
 import RPi.GPIO as GPIO
 import time
@@ -9,13 +13,26 @@ GPIO.setup(RPi_map.ENCODER_A1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(RPi_map.ENCODER_B0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(RPi_map.ENCODER_B1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+
+contador1 = 0
+contador2 = 0
+
 def mi_funcion_interrupcion(channel):
-    print("Interrupción detectada en el pin", channel)
+    if channel == 31:
+        global contador1
+        contador1 += 1
+        print("contador 1 = " + str(contador1))
+    elif channel == 22:
+        global contador2
+        contador2 += 1
+        print("contador 2 = " + str(contador2))
     
 # Configurar la interrupción en el pin especificado (flanco de subida)
 GPIO.add_event_detect(RPi_map.ENCODER_B0, GPIO.RISING, callback=mi_funcion_interrupcion)
 GPIO.add_event_detect(RPi_map.ENCODER_B1, GPIO.RISING, callback=mi_funcion_interrupcion)
+GPIO.add_event_detect(RPi_map.ENCODER_A0, GPIO.RISING, callback=mi_funcion_interrupcion)
 GPIO.add_event_detect(RPi_map.ENCODER_A1, GPIO.RISING, callback=mi_funcion_interrupcion)
+
 
 while True:
     time.sleep(1)
---
+++ src/pruebas/motor.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import RPi_map
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(RPi_map.CLKWA0, GPIO.OUT)
+GPIO.setup(RPi_map.CLKWA1, GPIO.OUT)
+GPIO.setup(RPi_map.CLKWB0, GPIO.OUT)
+GPIO.setup(RPi_map.CLKWB1, GPIO.OUT)
+
+# Configura los pines GPIO como salidas PWM
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(33, GPIO.OUT)
+
+# Configura la frecuencia PWM (en Hz)
+pwm_frequency = 1000  # Puedes ajustar esta frecuencia según tus necesidades
+
+# Crea objetos PWM para los pines GPIO
+pwm_12 = GPIO.PWM(12, pwm_frequency)
+pwm_33 = GPIO.PWM(33, pwm_frequency)
+
+# Inicia el PWM con un ciclo de trabajo del 100% (máximo)
+pwm_12.start(100)
+pwm_33.start(100)
+
+while(True):
+    
+    GPIO.output(RPi_map.CLKWA0,1)
+    GPIO.output(RPi_map.CLKWA1,0)        
+    GPIO.output(RPi_map.CLKWB0,0)
+    GPIO.output(RPi_map.CLKWB1,1)
