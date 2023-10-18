c741472007063db73e4a50bd581b2af37bfb7821
leoborgnino <leoborgnino@gmail.com>
2023-10-18 12:29:12 -0300

    Creacion  de clase motor y avanzar leyendo el encoder en el main

--- src/main_robertite.py
+++ src/main_robertite.py
@@ -1,20 +1,37 @@
 import sys
 sys.path.append("./data/")
 sys.path.append("./utils/")
-from Mpu6050 import Mpu6050
+sys.path.append("./movement/")
+
 import time
-import RPi.GPIO as GPIO
-from threading import Lock
 import matplotlib.pyplot as plt
 import datetime
+
+from threading import Lock
+import RPi.GPIO as GPIO
+
+from Mpu6050 import Mpu6050
 from Encoder import Encoder
 from Rplidar import Rplidar
+from motor import Motor
 
 scan_data_lock = Lock()
 
 encoder = Encoder()
-giro = Mpu6050()
-rplidar = Rplidar(scan_data_lock)
+motor = Motor()
 
-rplidar.stop_motor()
+motor.avanzar()
+
+while encoder.contador1<40:
+    #print("Avanzando")
+    pass
+
+print ("terminado")
+
+motor.avanzar(0,0)
+#giro = Mpu6050()
+#rplidar = Rplidar(scan_data_lock)
+
+#rplidar.stop_motor()
+#rplidar.cleanup()
 
--- src/movement/motor.py
+++ src/movement/motor.py
@@ -1,30 +1,44 @@
 import RPi.GPIO as GPIO
 import RPi_map
 
-GPIO.setmode(GPIO.BOARD)
+class Motor():
+    
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(RPi_map.CLKWA0, GPIO.OUT)
-GPIO.setup(RPi_map.CLKWA1, GPIO.OUT)
-GPIO.setup(RPi_map.CLKWB0, GPIO.OUT)
-GPIO.setup(RPi_map.CLKWB1, GPIO.OUT)
+        GPIO.setup(RPi_map.CLKWA0, GPIO.OUT)
+        GPIO.setup(RPi_map.CLKWA1, GPIO.OUT)
+        GPIO.setup(RPi_map.CLKWB0, GPIO.OUT)
+        GPIO.setup(RPi_map.CLKWB1, GPIO.OUT)
 
 
-# Configura los pines GPIO como salidas PWM
-GPIO.setup(12, GPIO.OUT)
-GPIO.setup(33, GPIO.OUT)
+        # Configura los pines GPIO como salidas PWM
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.setup(33, GPIO.OUT)
+        # Configura la frecuencia PWM (en Hz)
+        pwm_frequency = 1000  # Puedes ajustar esta frecuencia según tus necesidades
 
-# Configura la frecuencia PWM (en Hz)
-pwm_frequency = 1000  # Puedes ajustar esta frecuencia según tus necesidades
+        # Crea objetos PWM para los pines GPIO
+        self.pwm_12 = GPIO.PWM(12, pwm_frequency)
+        self.pwm_33 = GPIO.PWM(33, pwm_frequency)
 
-# Crea objetos PWM para los pines GPIO
-pwm_12 = GPIO.PWM(12, pwm_frequency)
-pwm_33 = GPIO.PWM(33, pwm_frequency)
 
-# Inicia el PWM con un ciclo de trabajo del pot% (máximo)
-pwm_12.start(50)
-pwm_33.start(50)
+    def avanzar(self,pwm1=50,pwm2=50):
 
-GPIO.output(RPi_map.CLKWA0,1)
-GPIO.output(RPi_map.CLKWA1,0)        
-GPIO.output(RPi_map.CLKWB0,0)
-GPIO.output(RPi_map.CLKWB1,1)
+        # Inicia el PWM con un ciclo de trabajo del pot% (máximo)
+        self.pwm_12.start(abs(pwm1))
+        self.pwm_33.start(abs(pwm2))
+
+        if (pwm1 > 0):
+            GPIO.output(RPi_map.CLKWA0,1)
+            GPIO.output(RPi_map.CLKWA1,0)
+        else:
+            GPIO.output(RPi_map.CLKWA0,0)
+            GPIO.output(RPi_map.CLKWA1,1) 
+            
+        if (pwm2 > 0):
+            GPIO.output(RPi_map.CLKWB0,0)
+            GPIO.output(RPi_map.CLKWB1,1)
+        else:
+            GPIO.output(RPi_map.CLKWB0,1)
+            GPIO.output(RPi_map.CLKWB1,0)
